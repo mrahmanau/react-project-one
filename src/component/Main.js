9a9047eaa38bec8ff1eda9cdbc10e5535e2967e4
mrahmanau <mrahmanlinks@gmail.com>
2023-02-08 02:23:29 -0400

    first commit

---
+++ src/component/Main.js
@@ -0,0 +1,5 @@
+function Main() {
+    return(<h1>Main content goes here.</h1>)
+}
+
+export default Main;
---
+++ src/component/Navbar.js
@@ -0,0 +1,16 @@
+export default function Navbar() {
+    return(
+        <header>
+            <div className="logo">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/768px-React-icon.svg.png?20220125121207" alt="ReactLogo" />
+                <a href="index.html">ReactFacts</a>
+            </div>
+            
+            <nav>
+                <ul>
+                    <li>React Course - Project 1</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
